d5291400b1f470db73577c5793885f7dc4205564
Pratham Dogra <dogra.pratham@gmail.com>
2019-04-02 22:16:33 +0530

    Final Commit

--- src/components/resume.js
+++ src/components/resume.js
@@ -11,15 +11,11 @@ class Resume extends Component {
       <div>
         <Grid>
           <Cell col={4}>
-            <div style={{textAlign: 'center'}}>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRi70CCgoE4USkla4_kcZfz9ldKackPMd3KBk2n5ni0cWN2IStC1g"
-                alt="avatar"
-                style={{height: '200px'}}
-                 />
+            <div className="centre">
+              <div className="avatar-img"></div>
             </div>
 
-            <h2 style={{paddingTop: '2em'}}>Pratham Dogra</h2>
+            <h2 style={{paddingTop: '1em'}}>Pratham Dogra</h2>
             <h4 style={{color: 'grey'}}>Web Developer</h4>
             <hr style={{borderTop: '3px solid #833fb2', width: '100%'}}/>
             <p>I am a programmer with intermediate knowledge in HTML, CSS and JavaScript. My programming journey began 5 years ago with C++, soon in college I switch to C(as C was in my college curriculum) and then eventually Python(worked with Tkinter and Scikit-learn). Around 3 months back I made a tryst with Front End web-development and got awestruck by its potential. Ever since I have worked on React, Django and Angular. This website is a glimpse of my work.</p>
